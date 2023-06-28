6c08c9d61ce4e9fa683e0be64f5ed6a2df8c1f21
TunedayCodes <fadipetunde5@gmail.com>
2023-06-28 12:42:14 +0100

    Latest Commmit to all three files

--- index.js
+++ index.js
@@ -5,3 +5,59 @@ burger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
   burger.classList.toggle('toggle');
 });
+
+// Variables
+const carouselContainer = document.querySelector('.carousel-container');
+const images = carouselContainer.querySelectorAll('img');
+const imageWidth = images[0].clientWidth;
+
+let currentIndex = 0;
+let timer;
+
+// Function to move to the next image
+function nextImage() {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  updateCarousel();
+}
+
+// Function to move to the previous image
+function prevImage() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  updateCarousel();
+}
+
+// Function to update the carousel position
+function updateCarousel() {
+  carouselContainer.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+}
+
+// Start the slideshow
+function startSlideshow() {
+  timer = setInterval(nextImage, 3000); // Change the duration (in milliseconds) as desired
+}
+
+// Stop the slideshow
+function stopSlideshow() {
+  clearInterval(timer);
+}
+
+// Event listeners for next and previous buttons
+document.getElementById('nextBtn').addEventListener('click', () => {
+  stopSlideshow();
+  nextImage();
+});
+document.getElementById('prevBtn').addEventListener('click', () => {
+  stopSlideshow();
+  prevImage();
+});
+
+// Start the slideshow initially
+startSlideshow();
+
+
